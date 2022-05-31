bc5aa0bb0fbb9f06c0ffd44b7eedffc27216a372
Noel Andia Flores <noel.andiaf@gmail.com>
2022-05-30 21:33:59 -0400

    Se añadió funcionalidad, nuevos campos y un poco de CSS

--- JS/descuentos.js
+++ JS/descuentos.js
@@ -7,3 +7,38 @@ function calcularPrecioConDescuento(precio, descuento) {
 
     return prConDesc;
 }
+
+function calcularPrecioPago(){
+    const precio = new Number(document.getElementById("InputPrice").value);
+    const descInput= new Number(document.getElementById("InputDiscount").value);
+    const descSelect = new Number(document.getElementById("selectDescuentos").value);
+
+    let precioPago = 0;
+
+    if (descInput > 0){
+        precioPago = calcularPrecioConDescuento(precio, descInput);
+    }else if(descSelect > 0){
+        precioPago = calcularPrecioConDescuento(precio, descSelect);
+    }else{
+        alert("Ha ocurrido un error al calcular el descuento");
+    }
+
+    document.getElementById("res-precio").innerText = `El precio que debes pagar es ${precioPago}`;
+}
+
+function limpiaDescuentoInput() {
+    const descInput = new Number(document.getElementById("InputDiscount").value);
+
+    if (descInput > 0){
+        document.getElementById("InputDiscount").value = "";
+    }
+}
+
+function limpiaDescuentoSelect() {
+    const descSelect = document.getElementById("selectDescuentos").value;
+
+    if (descSelect != "0"){
+        document.getElementById("selectDescuentos").value = 0;
+    }
+}
+
